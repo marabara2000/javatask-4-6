9b5072cd6ba6c20d9a1cabf1d85617c544ff0402
Marina B <balda558@gmail.com>
2024-09-29 16:54:16 +0300

    Add costructor

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -2,9 +2,21 @@ public class Radio {
 
     private int currentStation;
     private int currentVolume;
+    private int maxStation;
+
+
+    public Radio (){
+        this.maxStation = 9;
+
+    }
+
+    public Radio (int stationsCount){
+        this.maxStation = stationsCount - 1;
+
+    }
 
     public void next() {
-        if (currentStation != 9) {
+        if (currentStation != maxStation) {
             currentStation++;
         } else {
             currentStation = 0;
@@ -15,7 +27,7 @@ public class Radio {
         if (currentStation != 0) {
             currentStation--;
         } else {
-            currentStation = 9;
+            currentStation = maxStation;
         }
     }
 
@@ -27,7 +39,7 @@ public class Radio {
         if (currentStation < 0) {
             return;
         }
-        if (currentStation > 9) {
+        if (currentStation > maxStation) {
             return;
         }
         this.currentStation = currentStation;
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -6,6 +6,16 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class RadioTest {
 
+    @Test
+    public void test () {
+        Radio radio = new Radio (20);
+
+        radio.setCurrentStation(15);
+        int expected = 15;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected,actual);
+    }
+
 
     @Test
     public void shouldSetStationM() {
